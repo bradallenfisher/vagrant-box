225186cfdff3583abff2c43b33a5105e3bf4e759
Brad Fisher <bradallenfisher@gmail.com>
2015-09-01 10:46:46 -0400

    need the kernel crap

--- guest.sh
+++ guest.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
+yum install gcc kernel-devel kernel-headers dkms make bzip2 perl
 wget http://download.virtualbox.org/virtualbox/5.0.0_RC3/VBoxGuestAdditions_5.0.0_RC3.iso
 sudo mkdir /media/VBoxGuestAdditions
 sudo mount -o loop,ro VBoxGuestAdditions_5.0.0_RC3.iso /media/VBoxGuestAdditions
