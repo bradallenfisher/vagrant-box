c0c54a9cb5c9b107835d30149fa93dd3b23667cc
Brad Fisher <bradallenfisher@gmail.com>
2015-09-01 10:41:02 -0400

    duh

--- guest.sh
+++ guest.sh
@@ -2,8 +2,8 @@
 
 wget http://download.virtualbox.org/virtualbox/5.0.0_RC3/VBoxGuestAdditions_5.0.0_RC3.iso
 sudo mkdir /media/VBoxGuestAdditions
-sudo mount -o loop,ro VBoxGuestAdditions_4.3.8.iso /media/VBoxGuestAdditions
+sudo mount -o loop,ro VBoxGuestAdditions_5.0.0_RC3.iso /media/VBoxGuestAdditions
 sudo sh /media/VBoxGuestAdditions/VBoxLinuxAdditions.run
-rm VBoxGuestAdditions_4.3.8.iso
+rm VBoxGuestAdditions_5.0.0_RC3.iso
 sudo umount /media/VBoxGuestAdditions
 sudo rmdir /media/VBoxGuestAdditions
