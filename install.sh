0c3cb34b7840503cd70fe62ce5bf09d94257d360
Brad Fisher <bradallenfisher@gmail.com>
2015-09-01 15:53:50 -0400

    combo

--- install.sh
+++ install.sh
@@ -43,4 +43,13 @@ rm -rf /tmp/*
 
 rm -f /var/log/wtmp /var/log/btmp
 
-history -c
+rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
+yum install gcc kernel-devel kernel-headers dkms make bzip2 perl
+yum install kernel-devel-2.6.32-573.el6.x86_64
+wget http://download.virtualbox.org/virtualbox/5.0.0_RC3/VBoxGuestAdditions_5.0.0_RC3.iso
+sudo mkdir /media/VBoxGuestAdditions
+sudo mount -o loop,ro VBoxGuestAdditions_5.0.0_RC3.iso /media/VBoxGuestAdditions
+sudo sh /media/VBoxGuestAdditions/VBoxLinuxAdditions.run
+rm VBoxGuestAdditions_5.0.0_RC3.iso
+sudo umount /media/VBoxGuestAdditions
+sudo rmdir /media/VBoxGuestAdditions
