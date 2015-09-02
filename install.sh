7715c40d5987d834d5375a299b7b8474d42d22a4
Brad Fisher <bradallenfisher@gmail.com>
2015-09-01 22:23:35 -0400

    roll back the guest additions

--- install.sh
+++ install.sh
@@ -46,10 +46,10 @@ rm -f /var/log/wtmp /var/log/btmp
 rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
 yum install gcc kernel-devel kernel-headers dkms make bzip2 perl -y
 yum install kernel-devel-2.6.32-573.el6.x86_64 -y
-wget http://download.virtualbox.org/virtualbox/5.0.0_RC3/VBoxGuestAdditions_5.0.0_RC3.iso
+wget http://download.virtualbox.org/virtualbox/4.3.30/VBoxGuestAdditions_4.3.30.iso
 sudo mkdir /media/VBoxGuestAdditions
-sudo mount -o loop,ro VBoxGuestAdditions_5.0.0_RC3.iso /media/VBoxGuestAdditions
+sudo mount -o loop,ro VBoxGuestAdditions_4.3.30.iso /media/VBoxGuestAdditions
 sudo sh /media/VBoxGuestAdditions/VBoxLinuxAdditions.run
-rm VBoxGuestAdditions_5.0.0_RC3.iso -rf
+rm VBoxGuestAdditions_4.3.30.iso -rf
 sudo umount /media/VBoxGuestAdditions
 sudo rmdir /media/VBoxGuestAdditions
